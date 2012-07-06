0c8d1a4c6f7c60794ad03f5554891dae70d868d4
Pete Keen <peter.keen@bugsplat.info>
2012-07-06 11:27:31 -0700

    Whoa it works now

--- lib/speedee/app.rb
+++ lib/speedee/app.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra/base'
+require 'json'
 
 class Speedee::App < Sinatra::Base
 
@@ -8,6 +9,16 @@ class Speedee::App < Sinatra::Base
 
   get '/' do
     send_file File.join(settings.public_folder, 'index.html')
+  end
+
+
+  get '/api/threads' do
+    [
+      {id: 0, authors: "Pete, Mark, Sarah", subject: "Movie Night", date: "2012/01/01"},
+      {id: 1, authors: "Pete, Mark, Sarah", subject: "Movie Night", date: "2012/02/02"},
+      {id: 2, authors: "Pete, Mark, Sarah", subject: "Movie Night", date: "2012/03/03"},
+      {id: 3, authors: "Pete, Mark, Sarah", subject: "Movie Night", date: "2012/04/04"},
+    ].to_json
   end
   
 end
